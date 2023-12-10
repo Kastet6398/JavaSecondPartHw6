3dc8589df7da51bdabc73766ed06ee54efb99c73
Kastet6398 <konstantin.nezhalsky@gmail.com>
2023-12-10 20:40:46 +0200

    updated mudule-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ open module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.kordamp.bootstrapfx.core;
     requires java.desktop;
     requires java.sql;
     requires freemarker;
